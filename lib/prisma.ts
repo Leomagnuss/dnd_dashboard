a63b80e4c10ff3104fa5327d10d20ad6c0f642d8
root <root@vm1606080.vds.chsl.one>
2026-03-26 21:46:27 +0200

    + command.txt + styles

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,13 +2,10 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 
-// Создаём пул подключений к локальной базе
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 })
 
-// Создаём адаптер
 const adapter = new PrismaPg(pool)
 
-// Создаём клиент с адаптером
 export const prisma = new PrismaClient({ adapter })
